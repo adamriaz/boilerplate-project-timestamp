74a5b89a60f43591aa949bd1ebc365fc473a5440
Adam R <riaz_adam@hotmail.com>
2023-08-23 01:49:34 +0100

    Add test requirements

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +21,41 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", function (req, res) {
+  const params = req.params.date;
+  const INVALID_DATE = "Invalid Date"
+  const invalidResponseMessage = { error: INVALID_DATE }
 
+  const current = new Date();
+  const dateResponse = { unix: current.getTime(), utc: current.toUTCString() }
+  if (params === '' || params === undefined) {
+    res.json(dateResponse);
+  }
+
+  const date = new Date(params);
+
+  if (date.toString() === INVALID_DATE) {
+
+    const unix = new Date(parseInt(params, 10));
+    if (unix.toString() !== INVALID_DATE) {
+      dateResponse.unix = unix.getTime();
+      dateResponse.utc = unix.toUTCString();
+      res.json(dateResponse);
+    } else {
+      res.json(invalidResponseMessage);
+    }
+
+  } else {
+    dateResponse.unix = date.getTime();
+    dateResponse.utc = date.toUTCString();
+    res.json(dateResponse)
+  }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
